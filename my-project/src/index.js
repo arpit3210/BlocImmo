ca84f28fcd545cf4ccb2c319e503dbb81ffcad8d
1234 <arpitsinghthakur321@gmail.com>
2024-03-28 16:42:17 +0530

    adding explicitly

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -6,9 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your publishable key from the loaded environment
-const PUBLISHABLE_KEY = process.env.REACT_APP_VITE_CLERK_PUBLISHABLE_KEY;
+// const PUBLISHABLE_KEY = process.env.REACT_APP_VITE_CLERK_PUBLISHABLE_KEY;
 
 
+// Import your publishable key
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const PUBLISHABLE_KEY = "pk_test_ZXZvbHZlZC1jb3diaXJkLTQxLmNsZXJrLmFjY291bnRzLmRldiQ"
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
